10d65c8516d801e49ae23f027920db5efbfd1d1a
bond1995007 <Atul.Yadav@jda.com>
2018-07-06 14:34:17 +0530

    sum file added

---
+++ src/com/jda/functionalprograms/sum.java
@@ -0,0 +1,20 @@
+package com.jda.functionalprograms;
+
+import com.jda.utility.Utility;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		Utility utility =  new Utility();
+		a = utility.inputInteger();
+		b = utility.inputInteger();
+		System.out.println(a);
+		System.out.println(b);
+		
+
+	}
+
+}
